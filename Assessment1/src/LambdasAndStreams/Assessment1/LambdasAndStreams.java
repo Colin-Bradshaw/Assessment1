9c9231a78f87a3c343956c1b8843355dc1ef891b
Colin Bradshaw <colin.bradshaw@smoothstack.com>
2021-09-20 13:28:25 -0400

    removed trailing comma in part 2 of lambdas and streams more comments for readability

--- Assessment1/src/LambdasAndStreams/Assessment1/LambdasAndStreams.java
+++ Assessment1/src/LambdasAndStreams/Assessment1/LambdasAndStreams.java
@@ -20,24 +20,30 @@ public class LambdasAndStreams {
 	 */
 	public static void main(String[] args) {
 		System.out.println("---------------------------------------Part 1---------------------------------------");
+		// some pre-made lists to work as inputs for lambdas
 		List<String> walrus = Arrays.asList("Goo goo g' joob", "Sitting on a cornflake", "Waiting for the van to come",
 				"Corporation tee shirt", "Stupid bloody Tuesday", "Man, you been a naughty boy", "You let your face grow long");
 		List<Integer> evenAndOdd = Arrays.asList(1, 2, 3, 5, 88, 123);
 		List<String> stringList = Arrays.asList("a23", "555", "666", "a234");
 		
 		System.out.println("Shortest to longest\n------------------------------");
+		// making comparator in a different line for readability
 		Comparator<String> comp = (str1, str2) -> str1.length() - str2.length();
 		walrus.stream().sorted(comp).collect(Collectors.toList()).forEach((value) -> System.out.println(value));
 		
 		System.out.println("\nLongest to shortest\n-----------------------------");
+		// same comparator as above, but result is multiplied by -1
 		comp = (str1, str2) -> (str1.length() - str2.length()) * -1;
 		walrus.stream().sorted(comp).collect(Collectors.toList()).forEach((value) -> System.out.println(value));
 		
 		System.out.println("\nAlphabetically by first character\n-----------------------------");
-		comp = (str1, str2) -> (str1.charAt(0) - str2.charAt(0));
+		// similar math, ASCII values let subtraction do the sorting
+		// evaluating all characters as upper case such that A is not greater then a
+		comp = (str1, str2) -> (str1.toUpperCase().charAt(0) - str2.toUpperCase().charAt(0));
 		walrus.stream().sorted(comp).collect(Collectors.toList()).forEach((value) -> System.out.println(value));
 		
 		System.out.println("\nContaining 'e' first\n-----------------------------");
+		// still using comparator, but with an if statement
 		comp = (str1, str2) -> {
 			if(str1.toLowerCase().contains("e") && str2.toLowerCase().contains("e")) {
 				return 0;
@@ -70,9 +76,13 @@ public class LambdasAndStreams {
 				st.append('o').append(n).append(',');
 			}
 		});
-		System.out.println(st);
+		System.out.println(st.substring(0, st.length() -1));
+		
+
+		System.out.println("---------------------------------------Part 3---------------------------------------");
 		
 		System.out.println("\nFiltered list of Strings\n-----------------------------");
+		// using filter an stream to  get answer(s)
 		stringList.stream().filter(aString -> aString.length() == 3)
 				.filter(aString -> aString.charAt(0) == 'a')
 				.collect(Collectors.toList())
